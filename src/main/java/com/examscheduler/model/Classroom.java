2b0fb5097477ffb8f6ddf72656ee186abcbff3a9
Ahmet Emir Doğan <ahmetemirdogan04@gmail.com>
2025-12-03 22:50:34 +0300

    feat: add Classroom model with ID, capacity, and accommodation logic.

--- src/main/java/com/examscheduler/model/Classroom.java
+++ src/main/java/com/examscheduler/model/Classroom.java
@@ -1,4 +1,47 @@
 package com.examscheduler.model;
 
-public class Classroom {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Classroom implements Serializable {
+    private String classroomID;
+    private int capacity;
+
+    public Classroom(String classroomID, int capacity) {
+        this.classroomID = classroomID;
+        this.capacity = capacity;
+    }
+
+    public String getClassroomID() {
+        return classroomID;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public boolean canAccommodate(int studentCount) {
+        return capacity >= studentCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Classroom classroom = (Classroom) o;
+        return Objects.equals(classroomID, classroom.classroomID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(classroomID);
+    }
+
+    @Override
+    public String toString() {
+        return "Classroom{" +
+                "classroomID='" + classroomID + '\'' +
+                ", capacity=" + capacity +
+                '}';
+    }
 }
